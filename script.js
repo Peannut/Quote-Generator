c08b65df6e6b2885906af4dd761ad1942126d497
Peanut <47929413+zakarinox@users.noreply.github.com>
2021-03-15 18:41:01 +0100

    commit

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const quoteText = document.getElementById('quote-text'),
-    quoteTags = document.getElementById('quote-tags'),
-    quoteAuthor = document.getElementById('quote-author'),
-    genQuoteBtn = document.getElementById('gen-quote-btn');
+const quoteText = document.getElementById("quote-text"),
+    quoteTags = document.getElementById("quote-tags"),
+    quoteAuthor = document.getElementById("quote-author"),
+    genQuoteBtn = document.getElementById("gen-quote-btn");
 
 function randomQuote() {
-    fetch('https://api.quotable.io/random')
-        .then(response => response.json())
-        .then(data => {
+    fetch("https://api.quotable.io/random")
+        .then((response) => response.json())
+        .then((data) => {
             quoteText.textContent = data.content;
             quoteTags.textContent = data.tags;
             quoteAuthor.textContent = `-- ${data.author}`;
         });
 }
 randomQuote();
-genQuoteBtn.addEventListener('click', () => {
+genQuoteBtn.addEventListener("click", () => {
     randomQuote();
 });
